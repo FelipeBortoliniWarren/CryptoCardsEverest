601102f5e18000a95b82e9c9e15a95f17a751ae9
FelipeBortoliniWarren <felipe.bortolini@warren.com.br>
2022-09-15 17:26:31 -0300

    refactor: refatoração para rotas nomeadas

--- lib/movements/view/movements_page.dart
+++ lib/movements/view/movements_page.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_2/shared/utils/styles.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import '../../shared/widgets/bottom_nav_bar.dart';
+
 class MovementsPage extends StatelessWidget {
   const MovementsPage({super.key});
+
+  static const route = '/movements';
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +23,7 @@ class MovementsPage extends StatelessWidget {
           ],
         ),
       ),
+      bottomNavigationBar: const BottomNavBar(indexSelected: 1),
     );
   }
 }
--- lib/shared/widgets/bottom_nav_bar.dart
+++ lib/shared/widgets/bottom_nav_bar.dart
@@ -1,74 +1,57 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_application_2/portfolio/view/portfolio_page.dart';
 import 'package:flutter_application_2/shared/utils/styles.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-import '../../movements/view/movements_page.dart';
 import '../utils/assets.dart';
 
-class BottomNavBar extends StatefulWidget {
-  const BottomNavBar({super.key});
+class BottomNavBar extends StatelessWidget {
+  final int indexSelected;
 
-  @override
-  State<BottomNavBar> createState() => _BottomNavBarState();
-}
-
-class _BottomNavBarState extends State<BottomNavBar> {
-  int indexSelected = 0;
-  late PageController pageController;
-
-  @override
-  void initState() {
-    super.initState();
-    pageController = PageController(initialPage: indexSelected);
-  }
-
-  void changeIndex(int value) {
-    setState(() {
-      indexSelected = value;
-    });
-  }
-
-  final List<Widget> pages = [
-    const PortfolioPage(),
-    const MovementsPage(),
-  ];
+  const BottomNavBar({super.key, required this.indexSelected});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        onPageChanged: changeIndex,
-        controller: pageController,
-        children: pages,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: colorTextBlack,
-        onTap: (page) {
-          pageController.animateToPage(
-            page,
-            duration: const Duration(milliseconds: 400),
-            curve: Curves.ease,
-          );
-        },
-        selectedLabelStyle: bottomNavLabelsStyle(),
-        unselectedLabelStyle: bottomNavLabelsStyle(),
-        currentIndex: indexSelected,
-        items: [
-          BottomNavigationBarItem(
-            icon: const ImageIcon(AssetImage(warrenIcon), color: colorTextGrey,),
-            activeIcon: const ImageIcon(AssetImage(warrenIcon), color: pinkWarren,),
-            label: AppLocalizations.of(context)!.bottomBarPortfolio,
+    void changePage(int index) {
+      switch (index) {
+        case 0:
+          Navigator.pushNamed(context, '/portfolio');
+          break;
+        case 1:
+          Navigator.pushNamed(context, '/movements');
+          break;
+      }
+    }
+
+    return BottomNavigationBar(
+      selectedItemColor: colorTextBlack,
+      onTap: changePage,
+      selectedLabelStyle: bottomNavLabelsStyle(),
+      unselectedLabelStyle: bottomNavLabelsStyle(),
+      currentIndex: indexSelected,
+      items: [
+        BottomNavigationBarItem(
+          icon: const ImageIcon(
+            AssetImage(warrenIcon),
+            color: colorTextGrey,
           ),
-          BottomNavigationBarItem(
-            icon: const ImageIcon(AssetImage(centIcon), color: colorTextGrey,),
-            activeIcon: const ImageIcon(AssetImage(centIcon), color: pinkWarren,),
-            label: AppLocalizations.of(context)!.bottomBarMovements,
+          activeIcon: const ImageIcon(
+            AssetImage(warrenIcon),
+            color: pinkWarren,
           ),
-        ],
-      ),
+          label: AppLocalizations.of(context)!.bottomBarPortfolio,
+        ),
+        BottomNavigationBarItem(
+          icon: const ImageIcon(
+            AssetImage(centIcon),
+            color: colorTextGrey,
+          ),
+          activeIcon: const ImageIcon(
+            AssetImage(centIcon),
+            color: pinkWarren,
+          ),
+          label: AppLocalizations.of(context)!.bottomBarMovements,
+        ),
+      ],
     );
   }
 }
